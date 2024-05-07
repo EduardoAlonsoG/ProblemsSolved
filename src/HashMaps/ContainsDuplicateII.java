41b45dc602c888fea81083650ed13e6d5e062899
EAlonso <garcia.alonso.eduardo1@gmail.com>
2024-05-07 08:15:10 -0600

    sorting

--- src/HashMaps/ContainsDuplicateII.java
+++ src/HashMaps/ContainsDuplicateII.java
@@ -1,2 +1,46 @@
-package HashMaps;public class ContainsDuplicateII {
+package HashMaps;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ContainsDuplicateII {
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,1};
+        int k = 3;
+        System.out.println(
+                new SolutionCDII().containsNearbyDuplicate(nums, k)
+        );
+    }
 }
+class SolutionCDII {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Map<Integer , Integer> elementData = new HashMap<>();
+        for(int i = 0 ; i < nums.length ; i++){
+            if(!elementData.containsKey(nums[i])){
+                elementData.put(nums[i] , i);
+            }else{
+                if(i - elementData.get(nums[i]) <= k){
+                    return true;
+                }else{
+                    elementData.replace(nums[i], i);
+                }
+            }
+        }
+        return false;
+    }
+}
+
+/*
+Example 1:
+Input: nums = [1,2,3,1], k = 3
+Output: true
+
+Example 2:
+Input: nums = [1,0,1,1], k = 1
+Output: true
+
+Example 3:
+Input: nums = [1,2,3,1,2,3], k = 2
+Output: false
+
+*/
--- src/Sorting/LongestConsecutiveSequence.java
+++ src/Sorting/LongestConsecutiveSequence.java
@@ -1,16 +1,14 @@
-package HashMaps;
+package Sorting;
+
 import java.util.Arrays;
 
 public class LongestConsecutiveSequence {
     public static void main(String[] args) {
-        int[] nums = {100,4,200,1,3,2};
-        System.out.println(
-                new SolutionLCQ().longestConsecutive(nums)
-        );
+
     }
 }
 
-class SolutionLCQ {
+class SolutionLCS {
     public int longestConsecutive(int[] nums) {
         if (nums.length == 0) return 0;
         int maxseq = 0;
@@ -31,15 +29,3 @@ class SolutionLCQ {
         return maxseq;
     }
 }
-
-/*
-Example 1:
-Input: nums = [100,4,200,1,3,2]
-Output: 4
-Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.
-
-Example 2:
-Input: nums = [0,3,7,2,5,8,4,6,0,1]
-Output: 9
-
-*/
